07525c1919587dda7fa6d5675e56213f6911016d
realtobi999 <tobiasfilgas@gmail.com>
2024-05-25 13:06:34 +0200

    [built] password hasher class, unit tested

---
+++ src/LibrarySystem.Domain/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace LibrarySystem.Domain.Interfaces;
+
+public interface IPasswordHasher
+{
+    string Hash(string password);
+    bool Compare(string plainPassword, string hashedPassword);
+}
---
+++ src/LibrarySystem.Tests/Unit/PasswordHasherTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using LibrarySystem.Application.Services.Users;
+
+namespace LibrarySystem.Tests.Units;
+
+public class PasswordHasherTests
+{
+    [Fact]
+    public void PasswordHasher_Hash_Works()
+    {
+        // prepare
+        var hasher = new PasswordHasher();
+
+        var password = "TEST_PASSWORD_IN_PLAIN_TEXT";
+
+        // act & assert
+        var hashedPassword = hasher.Hash(password);
+
+        password.Should().NotBe(hashedPassword);
+    }
+
+    [Fact]
+    public void PasswordHasher_Compare_Works()
+    {
+        // prepare
+        var hasher = new PasswordHasher();
+
+        var password1 = "TEST_PASSWORD_IN_PLAIN_TEXT";
+        var password2 = "ANOTHER_TEST_PASSWORD";
+
+        var hashedPassword1 = hasher.Hash(password1);
+        var hashedPassword2 = hasher.Hash(password2);
+
+        // act & assert
+        hasher.Compare(password1, hashedPassword1).Should().BeTrue();
+        hasher.Compare(password2, hashedPassword2).Should().BeTrue();
+        hasher.Compare(password1, hashedPassword2).Should().BeFalse();
+        hasher.Compare(password2, hashedPassword1).Should().BeFalse();
+    }
+}

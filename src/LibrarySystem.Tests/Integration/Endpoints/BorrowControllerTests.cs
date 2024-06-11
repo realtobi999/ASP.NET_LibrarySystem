f14277347e4e1bb0edc2e71779de74811dc91750
realtobi999 <tobiasfilgas@gmail.com>
2024-06-11 21:25:56 +0200

    [test] integration tests for the borrow record create endpoint

---
+++ src/LibrarySystem.Tests/Integration/Endpoints/BorrowControllerTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Net.Http.Json;
+using System.Security.Claims;
+using FluentAssertions;
+using LibrarySystem.Domain.Dtos.Books;
+using LibrarySystem.Domain.Entities;
+using LibrarySystem.Domain.Exceptions;
+using LibrarySystem.Tests.Integration.Extensions;
+using LibrarySystem.Tests.Integration.Server;
+
+namespace LibrarySystem.Tests.Integration.Endpoints;
+
+public class BorrowControllerTests
+{
+    [Fact]
+    public async void BorrowController_CreateBorrow_Returns201AndLocationHeader()
+    {
+        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var user = new User().WithFakeData();
+        var book = new Book().WithFakeData();
+        var borrow = new Borrow().WithFakeData(book.Id, user.Id);
+
+        var token = JwtTokenTestExtensions.Create().Generate([
+            new Claim(ClaimTypes.Role, "Employee")
+        ]);
+
+        client.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", token));
+
+        var create1 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        var create2 = await client.PostAsJsonAsync("/api/book", book.ToCreateBookDto([], []));
+        create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        // act & assert
+        var response = await client.PostAsJsonAsync("/api/borrow", borrow.ToCreateBorrowDto());
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        var header = response.Headers.GetValues("Location");
+        header.Should().Equal(string.Format("/api/borrow/{0}", borrow.Id));
+    }
+
+    [Fact]
+    public async void BorrowController_CreateBorrow_BookIsLabeledAsUnavailable()
+    {
+        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var user = new User().WithFakeData();
+        var book = new Book().WithFakeData();
+        var borrow = new Borrow().WithFakeData(book.Id, user.Id);
+
+        var token = JwtTokenTestExtensions.Create().Generate([
+            new Claim(ClaimTypes.Role, "Employee")
+        ]);
+
+        client.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", token));
+
+        var create1 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        var create2 = await client.PostAsJsonAsync("/api/book", book.ToCreateBookDto([], []));
+        create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        // act & assert
+        var response = await client.PostAsJsonAsync("/api/borrow", borrow.ToCreateBorrowDto());
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        var get = await client.GetAsync(string.Format("/api/book/{0}", book.Id));
+        get.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        var content = await get.Content.ReadFromJsonAsync<BookDto>() ?? throw new DeserializationException();
+        content.Id.Should().Be(book.Id);
+        content.Available.Should().Be(false);
+    }
+
+    [Fact]
+    public async void BorrowController_CreateBorrow_Returns409WhenTryingToBorrowUnavailableBook()
+    {
+        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var user = new User().WithFakeData();
+        var book = new Book().WithFakeData();
+        var borrow1 = new Borrow().WithFakeData(book.Id, user.Id);
+        var borrow2 = new Borrow().WithFakeData(book.Id, user.Id);
+
+
+        var token = JwtTokenTestExtensions.Create().Generate([
+            new Claim(ClaimTypes.Role, "Employee")
+        ]);
+
+        client.DefaultRequestHeaders.Add("Authorization", string.Format("Bearer {0}", token));
+
+        var create1 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        var create2 = await client.PostAsJsonAsync("/api/book", book.ToCreateBookDto([], []));
+        create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        // act & assert
+        var create3 = await client.PostAsJsonAsync("/api/borrow", borrow1.ToCreateBorrowDto());
+        create3.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        var get = await client.GetAsync(string.Format("/api/book/{0}", book.Id));
+        get.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        var content = await get.Content.ReadFromJsonAsync<BookDto>() ?? throw new DeserializationException();
+        content.Id.Should().Be(book.Id);
+        content.Available.Should().Be(false);
+
+        var response = await client.PostAsJsonAsync("/api/borrow", borrow2.ToCreateBorrowDto());
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.Conflict);
+    }
+}
---
+++ src/LibrarySystem.Tests/Integration/Extensions/BorrowTestExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using LibrarySystem.Domain;
+using LibrarySystem.Domain.Dtos.Books;
+using LibrarySystem.Domain.Entities;
+
+namespace LibrarySystem.Tests.Integration.Extensions;
+
+public static class BorrowTestExtensions
+{
+    private static readonly Faker<Borrow> _borrowFaker = new Faker<Borrow>()
+        .RuleFor(u => u.Id, f => f.Random.Guid())
+        .RuleFor(u => u.BorrowDate, DateTimeOffset.UtcNow);
+
+    public static Borrow WithFakeData(this Borrow borrow, Guid bookId, Guid userId)
+    {
+        var fakeBorrow = _borrowFaker.Generate();
+        fakeBorrow.BookId = bookId;
+        fakeBorrow.UserId = userId;
+
+        return fakeBorrow;
+    }
+
+    public static CreateBorrowDto ToCreateBorrowDto(this Borrow borrow)
+    {
+        return new CreateBorrowDto
+        {
+            Id = borrow.Id,
+            BookId = borrow.BookId,
+            UserId = borrow.UserId,
+        };
+    }
+}

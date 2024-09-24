7b501c21daeac2bdea62394f232af39f372d1a40
realtobi999 <tobiasfilgas@gmail.com>
2024-09-24 19:38:00 +0200

    [fix] implemented pagination extension method for IEnumerable type + added a constants class with MAX_LIMIT_VALUE

--- src/LibrarySystem.Application/Core/Extensions/IEnumerableExtensions.cs
+++ src/LibrarySystem.Application/Core/Extensions/IEnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using LibrarySystem.Domain;
 using LibrarySystem.Domain.Exceptions.HTTP;
 
 namespace LibrarySystem.Application.Core.Extensions;
@@ -6,17 +7,14 @@ public static class IEnumerableExtensions
 {
     public static IEnumerable<T> Paginate<T>(this IEnumerable<T> source, int offset, int limit)
     {
-        if (offset == 0 || limit == 0)
-        {
-            return source;
-        }
+        if (offset < 0) throw new BadRequest400Exception("Offset must be zero or greater.");
 
-        if (offset < 0) throw new BadRequest400Exception("Offset must be greater than zero.");
-        if (limit < 0) throw new BadRequest400Exception("Limit must be greater than zero.");
+        if (limit < 0) throw new BadRequest400Exception("Limit must be zero or greater.");
 
-        source = source.Skip(offset);
-        source = source.Take(limit);
+        if (limit > Constants.MAX_LIMIT_VALUE) throw new BadRequest400Exception($"Limit must not exceed the maximum allowed value of {Constants.MAX_LIMIT_VALUE}.");
 
-        return source;
+        if (offset == 0 && limit == 0) return source;
+
+        return limit == 0 ? source.Skip(offset).Take(Constants.MAX_LIMIT_VALUE) : source.Skip(offset).Take(limit);
     }
 }
---
+++ src/LibrarySystem.Domain/Constants.cs
@@ -0,0 +1,6 @@
+namespace LibrarySystem.Domain;
+
+public class Constants
+{
+    public const int MAX_LIMIT_VALUE = 50;
+}

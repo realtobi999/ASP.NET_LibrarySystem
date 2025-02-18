cac5996f5c8169f19c586bdd03021bbf52159068
realtobi999 <tobiasfilgas@gmail.com>
2025-02-18 16:35:13 +0100

    [refactor] updated book review tests to use the database context instead of relying on GET endpoints + cleaning up the tests

--- src/LibrarySystem.Tests/Integration/Endpoints/BookReviewControllerTests.cs
+++ src/LibrarySystem.Tests/Integration/Endpoints/BookReviewControllerTests.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net.Http.Json;
 using System.Security.Claims;
-using LibrarySystem.Domain.Dtos.Books;
 using LibrarySystem.Domain.Dtos.Reviews;
+using LibrarySystem.Domain.Entities;
 using LibrarySystem.Presentation;
 using LibrarySystem.Tests.Integration.Factories;
 using LibrarySystem.Tests.Integration.Helpers;
@@ -12,166 +12,151 @@ namespace LibrarySystem.Tests.Integration.Endpoints;
 public class BookReviewControllerTests
 {
     [Fact]
-    public async void CreateBookReview_Returns201AndLocationHeader()
+    public async void CreateBookReview_Returns201AndBookReviewIsCreated()
     {
         // prepare
-        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var app = new WebAppFactory<Program>();
+        var client = app.CreateDefaultClient();
         var book = BookFactory.CreateWithFakeData();
         var user = UserFactory.CreateWithFakeData();
         var review = BookReviewFactory.CreateWithFakeData(book, user);
-        var token1 = JwtTestExtensions.Create().Generate([
+        var employeeToken = JwtTestExtensions.Create().Generate([
             new Claim(ClaimTypes.Role, "Employee")
         ]);
 
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token1}");
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {employeeToken}");
 
-        var create1 = await client.PostAsJsonAsync("/api/book", await book.ToCreateBookDtoWithGenresAndAuthorsAsync(client));
+        var author = AuthorFactory.CreateWithFakeData();
+        var genre = GenreFactory.CreateWithFakeData();
+
+        var create1 = await client.PostAsJsonAsync("/api/genre", genre.ToCreateGenreDto());
         create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-        var create2 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        var create2 = await client.PostAsJsonAsync("/api/author", author.ToCreateAuthorDto());
         create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create3 = await client.PostAsJsonAsync("/api/book", book.ToCreateBookDto([author.Id], [genre.Id]));
+        create3.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create4 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        create4.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
 
         // auth as the user to create the review
-        var token2 = JwtTestExtensions.Create().Generate([
+        var userToken = JwtTestExtensions.Create().Generate([
             new Claim(ClaimTypes.Role, "User"),
             new Claim("UserId", user.Id.ToString()),
         ]);
 
         client.DefaultRequestHeaders.Remove("Authorization");
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token2}");
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {userToken}");
 
         // act & assert
         var response = await client.PostAsJsonAsync("/api/review", review.ToCreateBookReviewDto());
         response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        // assert that the book review is created
+        using var context = app.GetDatabaseContext();
+        context.Set<BookReview>().Any(r => r.Id == review.Id).Should().BeTrue();
     }
 
     [Fact]
-    public async void DeleteBookReview_Returns204()
+    public async void UpdateBookReview_Returns204AndBookReviewIsUpdated()
     {
         // prepare
-        var client = new WebAppFactory<Program>().CreateDefaultClient();
+        var app = new WebAppFactory<Program>();
+        var client = app.CreateDefaultClient();
         var book = BookFactory.CreateWithFakeData();
         var user = UserFactory.CreateWithFakeData();
         var review = BookReviewFactory.CreateWithFakeData(book, user);
-        var token1 = JwtTestExtensions.Create().Generate([
+        var employeeToken = JwtTestExtensions.Create().Generate([
             new Claim(ClaimTypes.Role, "Employee")
         ]);
 
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token1}");
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {employeeToken}");
 
-        var create1 = await client.PostAsJsonAsync("/api/book", await book.ToCreateBookDtoWithGenresAndAuthorsAsync(client));
+        var author = AuthorFactory.CreateWithFakeData();
+        var genre = GenreFactory.CreateWithFakeData();
+
+        var create1 = await client.PostAsJsonAsync("/api/genre", genre.ToCreateGenreDto());
         create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-        var create2 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        var create2 = await client.PostAsJsonAsync("/api/author", author.ToCreateAuthorDto());
         create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create3 = await client.PostAsJsonAsync("/api/book", book.ToCreateBookDto([author.Id], [genre.Id]));
+        create3.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create4 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        create4.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
 
         // auth as the user to create the review
-        var token2 = JwtTestExtensions.Create().Generate([
+        var userToken = JwtTestExtensions.Create().Generate([
             new Claim(ClaimTypes.Role, "User"),
             new Claim("UserId", user.Id.ToString()),
         ]);
 
         client.DefaultRequestHeaders.Remove("Authorization");
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token2}");
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {userToken}");
 
-        var create3 = await client.PostAsJsonAsync("/api/review", review.ToCreateBookReviewDto());
-        create3.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-
-        // act & assert
-        var response = await client.DeleteAsync($"/api/review/{review.Id}");
-        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
-    }
-
-    [Fact]
-    public async void DeleteBookReview_Returns401WhenTryingToDeleteOtherUserReview()
-    {
-        // prepare
-        var client = new WebAppFactory<Program>().CreateDefaultClient();
-        var book = BookFactory.CreateWithFakeData();
-        var user = UserFactory.CreateWithFakeData();
-        var review = BookReviewFactory.CreateWithFakeData(book, user);
-        var token1 = JwtTestExtensions.Create().Generate([
-            new Claim(ClaimTypes.Role, "Employee")
-        ]);
-
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token1}");
-
-        var create1 = await client.PostAsJsonAsync("/api/book", await book.ToCreateBookDtoWithGenresAndAuthorsAsync(client));
-        create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-        var create2 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
-        create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-
-        // auth as the user to create the review
-        var token2 = JwtTestExtensions.Create().Generate([
-            new Claim(ClaimTypes.Role, "User"),
-            new Claim("UserId", user.Id.ToString()),
-        ]);
-
-        client.DefaultRequestHeaders.Remove("Authorization");
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token2}");
-
-        var create3 = await client.PostAsJsonAsync("/api/review", review.ToCreateBookReviewDto());
-        create3.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create5 = await client.PostAsJsonAsync("/api/review", review.ToCreateBookReviewDto());
+        create5.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
 
         // act & assert
-        var token3 = JwtTestExtensions.Create().Generate([
-            new Claim(ClaimTypes.Role, "User"),
-            new Claim("UserId", Guid.NewGuid().ToString()), // insert invalid user id
-        ]);
-
-        client.DefaultRequestHeaders.Remove("Authorization");
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token3}");
-
-        var response = await client.DeleteAsync($"/api/review/{review.Id}");
-        response.StatusCode.Should().Be(System.Net.HttpStatusCode.Unauthorized);
-    }
-
-    [Fact]
-    public async void UpdateBookReview_Returns204AndTextIsUpdated()
-    {
-        // prepare
-        var client = new WebAppFactory<Program>().CreateDefaultClient();
-        var book = BookFactory.CreateWithFakeData();
-        var user = UserFactory.CreateWithFakeData();
-        var review = BookReviewFactory.CreateWithFakeData(book, user);
-        var token1 = JwtTestExtensions.Create().Generate([
-            new Claim(ClaimTypes.Role, "Employee")
-        ]);
-
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token1}");
-
-        var create1 = await client.PostAsJsonAsync("/api/book", await book.ToCreateBookDtoWithGenresAndAuthorsAsync(client));
-        create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-        var create2 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
-        create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-
-        // auth as the user to create the review
-        var token2 = JwtTestExtensions.Create().Generate([
-            new Claim(ClaimTypes.Role, "User"),
-            new Claim("UserId", user.Id.ToString()),
-        ]);
-
-        client.DefaultRequestHeaders.Remove("Authorization");
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token2}");
-
-        var create3 = await client.PostAsJsonAsync("/api/review", review.ToCreateBookReviewDto());
-        create3.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-
-        // act & assert
-        var updateBookReviewDto = new UpdateBookReviewDto
+        var updateDto = new UpdateBookReviewDto
         {
             Text = "Test_Test_Test",
             Rating = review.Rating
         };
 
-        var response = await client.PutAsJsonAsync($"/api/review/{review.Id}", updateBookReviewDto);
+        var response = await client.PutAsJsonAsync($"/api/review/{review.Id}", updateDto);
         response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
 
-        var get = await client.GetAsync($"/api/book/{book.Id}");
-        get.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        // assert that the book review is created
+        using var context = app.GetDatabaseContext();
+        var updatedReview = context.Set<BookReview>().FirstOrDefault(r => r.Id == review.Id) ?? throw new NullReferenceException();
 
-        var content = await get.Content.ReadFromJsonAsync<BookDto>() ?? throw new NullReferenceException();
-        content.Id.Should().Be(book.Id);
-        content.Reviews.Count.Should().Be(1);
-        content.Reviews.ElementAt(0).Id.Should().Be(review.Id);
-        content.Reviews.ElementAt(0).Text.Should().Be(updateBookReviewDto.Text);
+        updatedReview.Id.Should().Be(review.Id);
+        updatedReview.Text.Should().Be(updateDto.Text);
+    }
+    [Fact]
+    public async void DeleteBookReview_Returns204AndBookReviewIsDeleted()
+    {
+        // prepare
+        var app = new WebAppFactory<Program>();
+        var client = app.CreateDefaultClient();
+        var book = BookFactory.CreateWithFakeData();
+        var user = UserFactory.CreateWithFakeData();
+        var review = BookReviewFactory.CreateWithFakeData(book, user);
+        var employeeToken = JwtTestExtensions.Create().Generate([
+            new Claim(ClaimTypes.Role, "Employee")
+        ]);
+
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {employeeToken}");
+
+        var author = AuthorFactory.CreateWithFakeData();
+        var genre = GenreFactory.CreateWithFakeData();
+
+        var create1 = await client.PostAsJsonAsync("/api/genre", genre.ToCreateGenreDto());
+        create1.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create2 = await client.PostAsJsonAsync("/api/author", author.ToCreateAuthorDto());
+        create2.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create3 = await client.PostAsJsonAsync("/api/book", book.ToCreateBookDto([author.Id], [genre.Id]));
+        create3.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+        var create4 = await client.PostAsJsonAsync("/api/auth/register", user.ToRegisterUserDto());
+        create4.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        // auth as the user to create the review
+        var userToken = JwtTestExtensions.Create().Generate([
+            new Claim(ClaimTypes.Role, "User"),
+            new Claim("UserId", user.Id.ToString()),
+        ]);
+
+        client.DefaultRequestHeaders.Remove("Authorization");
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {userToken}");
+
+        var create5 = await client.PostAsJsonAsync("/api/review", review.ToCreateBookReviewDto());
+        create5.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        // act & assert
+        var response = await client.DeleteAsync($"/api/review/{review.Id}");
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NoContent);
+
+        // assert that the book review is deleted
+        using var context = app.GetDatabaseContext();
+        context.Set<BookReview>().Any(r => r.Id == review.Id).Should().BeFalse();
     }
 }
